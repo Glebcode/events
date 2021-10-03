1b085777bff8205e00c8828d968032345a5fed71
Ihor Sofiychuk <i.sophiychuk@viseven.com>
2021-10-03 22:28:08 +0300

    update(*): corrections with events

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let redBox = document.getElementById('red');
 redBox.addEventListener('mousedown', mousDwFunc);
+redBox.addEventListener('mousemove', mousMoveFunc);
 
-function mousDwFunc (event) {
-console.log(event, 'EVENT')
-event.target.draggable = true;
-redBox.setAttribute('transform', 'matrix')
+function mousDwFunc(event) {
+    console.log(event, 'EVENT')
+}
+function mousMoveFunc(event) {
+    console.log(event,redBox.offsetTop, 'EVENT')
+    let y = `${redBox.offsetTop + event.y}`;
+    let x = `${event.x + redBox.offsetLeft  }`;
+
+    redBox.setAttribute('style', `top: ${y}px; left: ${x}px;`)
 }
