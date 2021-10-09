68c6a00d8f22e8de64da74dafb800e02c30d827f
Gleb Stachev <stachev.gleb@gmail.com>
2021-10-09 17:42:47 +0300

    update(js, css, html):add(sq, types, mouseDown)

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-let redBox = document.getElementById('red');
-redBox.addEventListener('mousedown', mousDwFunc);
+let box = document.querySelectorAll('.colorSq')
+let boxPlaces = document.getElementById('boxPlace');
+createBoxPlaces();
 
-function mousDwFunc (event) {
-console.log(event, 'EVENT')
-event.target.draggable = true;
-redBox.setAttribute('transform', 'matrix')
+function createBoxPlaces() {
+    for (let i = 0; i < 4; i++) {
+        boxPlaces.innerHTML += `<div class="typeBox sq"></div>`
+    }
 }
+
+let offsetY;
+let offsetX;
+
+box.forEach(function (item) {
+    return item.addEventListener('dragstart', function(event){
+        offsetX = event.offsetX;
+        offsetY = event.offsetY;
+        console.log('offsetX'+'-'+offsetX, 'offsetY'+'-'+offsetY);
+    }) 
+});
+      
+box.forEach(function (item) {
+        return item.addEventListener('dragend', function(event){
+          item.style.top = (event.pageY-offsetY) + 'px';
+          item.style.left = (event.pageX-offsetX) + 'px';
+          console.log('eventpageX'+'-'+event.pageX, 'eventpageY'+'-'+event.pageY);
+          console.log('eventX'+'-'+item.style.left, 'eventY'+'-'+item.style.top);
+      });
+});
+// redBox.addEventListener('dragstart', function(event){
+//   offsetX = event.offsetX;
+//   offsetY = event.offsetY;
+//   console.log('offsetX'+'-'+offsetX, 'offsetY'+'-'+offsetY);
+// })
+
+// redBox.addEventListener('dragend', function(event){
+//     redBox.style.top = (event.pageY-offsetY) + 'px';
+//     redBox.style.left = (event.pageX-offsetX) + 'px';
+//     console.log('eventpageX'+'-'+event.pageX, 'eventpageY'+'-'+event.pageY);
+//     console.log('eventX'+'-'+redBox.style.left, 'eventY'+'-'+redBox.style.top);
+// })
+
+
+// redBox.addEventListener('mousedown', mousDwFunc);
+
+// function mousDwFunc (event) {
+// console.log(event, 'EVENT')
+// event.target.draggable = true;
+// redBox.setAttribute('transform', 'matrix')
+// }
+
+
+
