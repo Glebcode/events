9aafe7b5291a5ee7aca4ccf52da058595a85fefa
Gleb Stachev <stachev.gleb@gmail.com>
2021-10-09 19:02:35 +0300

    update(js):add(new events)

--- index.js
+++ index.js
@@ -10,24 +10,38 @@ function createBoxPlaces() {
 
 let offsetY;
 let offsetX;
+let inDrag = false;
 
 box.forEach(function (item) {
-    return item.addEventListener('dragstart', function(event){
+    return item.addEventListener('mousedown', function(event){
         offsetX = event.offsetX;
         offsetY = event.offsetY;
-        // item.style.cursor = 'grabbing';
+        item.style.cursor = 'grabbing';
+        inDrag = true;
         console.log('offsetX'+'-'+offsetX, 'offsetY'+'-'+offsetY);
     }) 
 });
+
+box.forEach(function (item) {
+    return item.addEventListener('mousemove', function(event){
+      if(!inDrag){
+          return
+      }
+      item.style.top = (event.pageY-offsetY) + 'px';
+      item.style.left = (event.pageX-offsetX) + 'px';
+      item.style.cursor = 'grabbing';
+      console.log('eventpageX'+'-'+event.pageX, 'eventpageY'+'-'+event.pageY);
+      console.log('eventX'+'-'+item.style.left, 'eventY'+'-'+item.style.top);
+  });
+});
       
 box.forEach(function (item) {
-        return item.addEventListener('dragend', function(event){
+        return item.addEventListener('mouseup', function(event){
           item.style.top = (event.pageY-offsetY) + 'px';
           item.style.left = (event.pageX-offsetX) + 'px';
-        //   item.style.cursor = 'grabbing';
-          console.log('eventpageX'+'-'+event.pageX, 'eventpageY'+'-'+event.pageY);
-          console.log('eventX'+'-'+item.style.left, 'eventY'+'-'+item.style.top);
-      });
+          inDrag = false;
+          console.log(event.target);
+        });
 });
 // redBox.addEventListener('dragstart', function(event){
 //   offsetX = event.offsetX;
